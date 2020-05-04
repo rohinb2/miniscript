d9115fc75ab1755c0b1e5b37922f593f798513ef
Max Göttlicher <maxgoett@yahoo.de>
2020-05-05 00:02:07 +0200

    parser recognizes grammar

--- miniscript_ast.py
+++ miniscript_ast.py
@@ -1,11 +1,25 @@
+from typing import Optional
+
 
 class NodeVisitor:
-    def visit_Operator(self, o: 'Operator'): pass
-    def visit_Null(self, n: 'Null'): pass
-    def visit_Number(self, n: 'Number'): pass
-    def visit_Boolean(self, b: 'Boolean'): pass
-    def visit_String(self, s: 'String'): pass
-    def visit_Symbol(self, s: 'Symbol'): pass
+    def visit_Operator(self, o: "Operator"):
+        pass
+
+    def visit_Null(self, n: "Null"):
+        pass
+
+    def visit_Number(self, n: "Number"):
+        pass
+
+    def visit_Boolean(self, b: "Boolean"):
+        pass
+
+    def visit_String(self, s: "String"):
+        pass
+
+    def visit_Symbol(self, s: "Symbol"):
+        pass
+
 
 class AST:
     def __init__(self):
@@ -14,53 +28,148 @@ class AST:
     def visit(self, visitor: NodeVisitor):
         pass
 
-    def __str__(self):
-        self._str_prec(0)
-
-class Expr(AST):
+class Stmt(AST):
     pass
 
-class Operator(Expr):
-    def __init__(self, left, right, op):
-        self.left = left
-        self.right = right
-        self.op = op
-    
-    def visit(self, v: NodeVisitor):
-        v.visit_operator(self)
-    
-    _prec = {'+':17, '-': 17, '*': 16, '/': 16, '&&': 6, '||': 5}
-    @classmethod
-    def precedence(cls.op):
-        return cls._prec.get(cls, op, 0)
+class Expr(Stmt):
+    def __str__(self) -> str:
+        return self.str_prec(0) + ';'
 
-    def _str_prec(self, prec):
-        pass
+    def str_prec(self, precedence: int) -> str:
+        return "Ast"
+
+
+class If(Stmt):
+    def __init__(self, cond: Expr, then: Stmt, els: Optional[Stmt] = None):
+        self.cond = cond
+        self.then = then
+        self.els = els
+
+    def __repr__(self):
+        elstring = ''
+        if self.els is not None:
+            elstring = f', {repr(self.els}'
+        return f'{type(self).__name__}({repr(self.cond)}, {repr(self.then)}{elstring})'
+    
+    def __str__(self):
+        ifstring = r'if ({self.cond}) {\nself.then}'
+        if self.els is not None:
+            ifstring += r' else {self.els}'
+
+class While(Stmt):
+    def __init__(self, cond: Expr, body: Stmt):
+        self.cond = cond
+        self.body = body
+
+class BinOp(Expr):
+    def __init__(self, op: str, left: AST, right: AST):
+        self.left: AST = left
+        self.right: AST = right
+        print(type(self.left), type(self.right))
+        self.op: AST = op
+
+    def visit(self, v: NodeVisitor) -> None:
+        v.visit_operator(self)
+
+    _prec = {
+        '&&':6,
+        '||':5,
+        '==':11,
+        '!=':11,
+        '>': 12,
+        '<': 12,
+        '>=':12,
+        '<=':12,
+        '+': 14,
+        '-': 14,
+        '*': 15,
+        '/': 15,
+        '%': 15,
+        '!': 17,
+    }
+    # setting _assoc to None results in same str result of
+    # (a && b) && c == a && b && c == a && (b && c)
+    _assoc = {'!': 'left', '&&': None, '||': None, '+': None}
+
+    @classmethod
+    def precedence_of(cls, op: Optional[str] = None, side: Optional[str] = None) -> int:
+        return cls._prec.get(op, 0)
+
+    def inner_precedence(self, side: Optional[str] = None) -> int:
+        if side is not None and side != self._assoc.get(self.op, 'left'):
+            return max(self.precedence_of(self.op) + 1, 0)
+        return self.precedence_of(self.op, side)
+
+    @property
+    def precedence(self) -> int:
+        return self.precedence_of(self.op)
+
+    def str_prec(self, prec: int) -> str:
+        left = self.left.str_prec(self.inner_precedence('left'))
+        right = self.right.str_prec(self.inner_precedence('right'))
+        s = f'{left} {self.op} {right}'
+        if self.precedence < prec:
+            return f"({s})"
+        else:
+            return s
+
+    def __repr__(self):
+        return f"{type(self).__name__}({repr(self.op)}, {repr(self.left)}, {repr(self.right)})"
+    
+class Not(Expr):
+    def __init__(self, expr: Expr):
+        self.expr = expr
+    
+    def __repr__(self):
+        return r'{type(self).__name__}({self.expr})'
+    
+    def str_prec(self, prec: int):
+        return f'!{self.expr.str_prec(BinOp.precedence_of('!')}'
+
 
 class Literal(Expr):
     def __init__(self, value):
         self.value = value
-    
+
+    def str_prec(self, prec: int) -> str:
+        return str(self.value)
+
+    def __repr__(self):
+        return f"{type(self).__name__}({repr(self.value)})"
+
+
 class String(Literal):
     def visit(self, v: NodeVisitor):
         v.visit_String(self)
+
+    def str_prec(self, prec):
+        return f'"{self.value}""'
+
 
 class Boolean(Literal):
     def visit(self, v: NodeVisitor):
         v.visit_Boolean(self)
 
+
 class Number(Literal):
     def visit(self, v: NodeVisitor):
         v.visit_Number(self)
 
+
 class Null(Literal):
     def __init__(self):
         Literal.__init__(self, None)
-    
-    def visit(self, v: NodeVisitor): v.visit_Null(self)
+
+    def visit(self, v: NodeVisitor):
+        v.visit_Null(self)
+
+    def str_prec(self, prec):
+        return "null"
+
 
 class Symbol(Expr):
     def __init__(self, name):
         self.name = name
 
-    def visit(self, v: NodeVisitor): v.visit_String(self)
+    def visit(self, v: NodeVisitor):
+        v.visit_String(self)
--- parser.py
+++ parser.py
@@ -4,15 +4,22 @@ from miniscript_ast import *
 
 
 class MiniScriptLexer(Lexer):
-    tokens = { NULL, NUMBER, STRING, BOOLEAN, 
-        OPERATOR,
-        #PLUS, MINUS, TIMES, DIVIDE, 
-        #SEMICOLON,
-        IDENTIFIER,
-        IF, ELSE, WHILE, FOR, FUNCTION, VAR,
-        EQ, NE, GT, LT, GEQ, LEQ, ASSIGN
+    tokens = {
+        NULL,
+        NUMBER,
+        STRING,
+        BOOLEAN,
+        PLUS, MINUS, TIMES, DIV, AND, OR, EQ, NEQ, LT, LE, GT, GE,
+        ID,
+        IF,
+        ELSE,
+        WHILE,
+        FOR,
+        FUNCTION,
+        VAR,
+        ASSIGN,
     }
-    literals = {'(', ')', '{', '}', '[', ']', ';'}
+    literals = {'(', ')', '{', '}', '[', ']', ';', '!'}
 
     @_(r'\n+')
     def ignore_newline(self, t):
@@ -23,32 +30,36 @@ class MiniScriptLexer(Lexer):
     ignore_multicomment = r'\/\*([^*]|\*[^/])*\*\/'
 
     NULL = r'null'
+
     @_(r'\d+')
     def NUMBER(self, t):
         t.value = int(t.value)
         return t
-    
+
     @_(r'"(\\.|[^"\\])*"')
     def STRING(self, t):
         # TODO process escape sequences
         t.value = t.value[1:-1]
         return t
-    BOOLEAN = r'(true|false)'
-    IDENTIFIER = r'[_$\w][_$\w\d]*'
-    OPERATOR = r'\+|\-|\*|\/'
-    IF = 'if'
-    ELSE = 'else'
-    WHILE = 'while'
-    FOR = 'for'
-    FUNCTION = 'function'
-    VAR = 'var'
 
-    EQ, NE, GT, LT, GEQ, LEQ, ASSIGN = '==', '!=', '>', '<', '>=', '<=', '='
+    BOOLEAN = r'(true|false)'
+    ID = r'[_$\w][_$\w\d]*'
+    ID['function'] = FUNCTION
+    ID['if'] = IF
+    ID['else'] = ELSE
+    ID['while'] = WHILE
+    ID['for'] = FOR
+    ID['var'] = VAR
+    PLUS, MINUS, TIMES, DIV = r'\+', '-', r'\*', '/'
+    AND, OR = '&&', r'\|\|'
+    EQ, NEQ, LT, LE, GT, GE = '==', '!=', '<', '<=', '>', '>='
+    ASSIGN = '='
 
     def error(self, t):
-        print(f'{t.lineno}:{self.find_column(t.value, t)}: Illegal character: "{t.value[0]}". Ignoring...')
+        print(
+            f'{t.lineno}:{self.find_column(t.value, t)}: Illegal character: "{t.value[0]}". Ignoring...'
+        )
         self.index += 1
-
 
     @staticmethod
     def find_column(text, token):
@@ -58,36 +69,163 @@ class MiniScriptLexer(Lexer):
         column = (token.index - last_cr) + 1
         return column
 
-class MiniScriptParser(Parser):
-    tokens = MiniScriptLexer.tokens
 
-    @_('expr OPERATOR expr')
-    def expr(self, p):
-        return Operator(p.expr0, p.expr1, p.OPERATOR)
+class MiniScriptParser(Parser):
+    """Parser for the miniscript language.
+    miniscript has the following grammar:
+
+    prog : stmt prog | fun_decl prog | empty
+
+    fun_decl : "function" ID ( [arg [, arg]*] ) { stmt_list }
+
+    stmt_list : stmt stmt_list | empty
+
+    stmt : expr ;
+        | if ( expr ) stmt [else stmt]
+        | while ( expr ) stmt
+        | { stmt_list }
     
+    expr : expr op expr | ! expr | ( expr ) 
+        | ID | NUM | STRING | true | false | null
+
+    op : + | - | * | / | && | || | == | <= | => | < | > 
+    """
+    debugfile = 'parser.out'
+    tokens = MiniScriptLexer.tokens
+    precedence = (
+        ('right', AND),
+        ('right', OR),
+        ('left', EQ, NEQ),
+        ('left', GT, LT, GE, LE),
+        ('left', PLUS, MINUS),
+        ('left', TIMES, DIV),
+    )
+
+    @_('stmt prog')
+    def prog(self, p):
+        return  # TODO
+
+    @_('func prog')
+    def prog(self, p):
+        return # TODO
+    
+    @_('empty')
+    def prog(self, p):
+        pass # TODO
+
+    @_('stmt stmt_list')
+    def stmt_list(self, p):
+        return [p.stmt] + p.stmt_list
+    
+    @_('empty')
+    def stmt_list(self, p):
+        return []
+    
+    # TODO
+    @_('FUNCTION ID "(" args ")" "{" stmt_list "}"')
+    def func(self, p):
+        pass # TODO
+
+    @_('ID args2')
+    def args(self, p):
+        return [p.ID] + p.args2
+
+    @_('empty')
+    def args(self, p):
+        return []
+    
+    @_(', ID args2')
+    def args2(self, p):
+        return [p.ID] + p.args2
+    
+    @_('empty')
+    def args2(self, p):
+        return []
+
+    @_('expr ";"')
+    def stmt(self, p):
+        return
+    
+    @_('empty ";"')
+    def stmt(self, p):
+        return
+    
+    @_('block')
+    def stmt(self, p):
+        return [p.stmt]
+
+    @_('ifthenelse')
+    def stmt(self, p):
+        pass # TODO
+
+    @_('whileloop')
+    def stmt(self, p):
+        pass # TODO
+
+    @_('IF "(" expr ")" stmt ELSE stmt')
+    def ifthenelse(self, p):
+        pass # TODO
+
+    @_('IF "(" expr ")" stmt')
+    def ifthenelse(self, p):
+        pass # TODO
+    
+    @_('"{" stmt_list "}"')
+    def block(self, p):
+        return p.stmt_list
+    
+    @_('WHILE "(" expr ")" block')
+    def whileloop(self, p);
+        pass # TODO
+
+    @_(*(f'expr {op} expr' for op in [PLUS, MINUS, TIMES, DIV, AND, OR, EQ, NEQ, LT, LE, GT, GE]))
+    def expr(self, p):
+        return BinOp(p[1], p.expr0, p.expr1)
+
     @_('"(" expr ")"')
-    def expr(self, p): return p.expr
+    def expr(self, p):
+        return p.expr
+    
+    @_('"!" expr')
+    def expr(self, p):
+        pass # TODO
 
     @_('NULL')
-    def expr(self, p): return Null
+    def expr(self, p):
+        return Null
 
     @_('NUMBER')
-    def expr(self, p): return Number(p.NUMBER)
+    def expr(self, p):
+        return Number(p.NUMBER)
 
     @_('STRING')
-    def expr(self, p): return String()
+    def expr(self, p):
+        return String()
 
-    @_('IDENTIFIER')
-    def expr(self, p): return Symbol(p[0])
+    @_('ID')
+    def expr(self, p):
+        return Symbol(p[0])
 
-if __name__ == "__main__":
+    @_('BOOLEAN')
+    def expr(self, p):
+        return Boolean(p.BOOLEAN)
+
+    @_('')
+    def empty(self, p):
+        return []
+
+
+if __name__ == '__main__':
     lexer = MiniScriptLexer()
     parser = MiniScriptParser()
     code = ''
     while True:
-        try: 
+        try:
             code += input() + '\n'
             print(repr(code))
         except EOFError:
             break
-    print(parser.parse(lexer.tokenize(code)))
+    print(list(lexer.tokenize(code)))
+    tree = parser.parse(lexer.tokenize(code))
+    print(tree)
+    print(repr(tree))
